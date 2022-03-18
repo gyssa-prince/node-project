d22bff7cccff23a57cb8e2a8fc2381a4f4f7d389
gyssa-prince <91846911+gyssa-prince@users.noreply.github.com>
2022-03-18 09:44:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,20 +9,11 @@ app.use(bodyParser.json());
 import blogsRoute from './routes/blogs.js';
 import loginRoute from './routes/auth.js';
 
-//Enable CORS
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
-
 //ROUTES
-app.get("/", function (req, res, next) {
+app.get("/", function (req, res) {
   res.send("We are on home!!!");
 });
-  app.post('/', function(req, res, next) {
-    // Handle the post for this route
-  })
+
 app.use('/blogs', blogsRoute);
 app.use('/login', loginRoute);
 
