350d87f1b1714ba5635a2b92743dca3a9aa6f4b1
gyssa-prince <91846911+gyssa-prince@users.noreply.github.com>
2022-03-24 18:41:29 +0200

    Update blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,63 +1,4 @@
 import express from 'express';
-import blogs from '../models/blogs.js';
-const router = express.Router();
-import Blog from '../models/blogs.js';
-import verify from '../verify.js';
-
-
-
-//To get all blogs
-router.get('/', async(req, res)=>{
-    try{
-     const blogs = await Blog.find();
-     res.json(blogs);
-    }catch(err){
-    res.json({message:err});
-    res.status(501);
-    }
-});
-
-//To get one specific blog
-router.get('/:blogId',async(req,res)=>{
-   try{
-        const blog = await Blog.findById(req.params.blogId);
-        if(blog==null){
-            res.sendStatus(404);
-        }else{
-        res.json(blog);
-    }
-}catch(err){
-    res.json({message:err});
-    res.status(501);
-}
-});
-
-//To delete a post
-router.delete('/:blogId',verify, async(req,res)=>{
-    const blog = await Blog.findById(req.params.blogId);
-    if(blog!=null){try{
-        await Blog.remove({_id: req.params.blogId});
-        res.json("Blog deleted");
-    }catch(err){
-        res.json({message:err});
-        res.status(501);
-    }}else{
-        res.sendStatus(404);
-    }
-    });
-//To update a post
-router.patch('/:blogId',verify, async (req,res)=>{
-    try{
-        await Blog.updateOne({_id: req.params.blogId}, {$set: {Title: req.body.Title,Body:req.body.Body}});
-        res.json("Blog updated");
-    }catch(err){
-        res.json({message:err});
-        res.status(501);
-    }
-})
-
-
-import express from 'express';
 const router = express.Router();
 import Blog from '../models/blogs.js';
 import verify from '../verify.js';
@@ -125,12 +66,11 @@ router.post('/',verify, upload.single("file"), async(req,res) =>{
     try {
      console.log("req.body: ",req.body);
      if (req.file === undefined) return res.send("you must select a file.");
-    const imgUrl = `https://gyssablog.herokuapp.com/file/${req.file.filename}`;
+    const imgUrl = `http://localhost:3000/file/${req.file.filename}`;
 
      const newBlog = new Blog({
          Title: req.body.Title,
          Body: req.body.Body,
-         Dates:req.body.Date
          Picture: imgUrl
      });
      
