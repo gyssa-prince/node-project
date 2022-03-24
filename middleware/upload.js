7f4563960a90dda9030e97a6cd9c2f0ae7ed0057
gyssa-prince <91846911+gyssa-prince@users.noreply.github.com>
2022-03-24 17:46:08 +0200

    add upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import {GridFsStorage} from 'multer-gridfs-storage';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const storage = new GridFsStorage({
+    url: process.env.MONGODB_CONNECTION_STRING,
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+    file: (req, file) => {
+        const match = ["image/png", "image/jpeg"];
+
+        if (match.indexOf(file.mimetype) === -1) {
+            const filename = `${Date.now()}-any-name-${file.originalname}`;
+            return filename;
+        }
+
+        return {
+            bucketName: "photos",
+            filename: `${Date.now()}-any-name-${file.originalname}`,
+        };
+    },
+});
+
+const upload= multer({ storage });
+export default upload;
