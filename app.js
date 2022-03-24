895909d5978e158cd5942a89fc9164113c483796
gyssa-prince <91846911+gyssa-prince@users.noreply.github.com>
2022-03-24 17:50:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import Grid from 'gridfs-stream';
 
 dotenv.config();
 const app =express();
 app.use(bodyParser.json());
-app.use(cors());
 import blogsRoute from './routes/blogs.js';
 import loginRoute from './routes/auth.js';
 import messageRoute from './routes/messages.js';
 import subcribeRoute from './routes/subcribe.js';
 import commentRoute from './routes/comments.js';
+import uploadRoute from './routes/upload.js';
 
-
-
+let gfs;
 //ROUTES
 app.get('/', (req,res)=>{
     res.send('We are on home');
@@ -25,6 +24,7 @@ app.use('/login', loginRoute);
 app.use('/message', messageRoute);
 app.use('/subcribe', subcribeRoute);
 app.use('/comment', commentRoute);
+
 
 
 //Connection to DB
@@ -38,6 +38,31 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
 const connection =mongoose.connection;
 connection.once("open",()=>{
     console.log('Mango DB is connected sucessfully')
+    gfs = Grid(connection.db, mongoose.mongo);
+    gfs.collection("photos");
 });
+
+// media routes
+app.get("/file/:filename", async (req, res) => {
+    try {
+        const file = await gfs.files.findOne({ filename: req.params.filename });
+        const readStream = gfs.createReadStream(file.filename);
+        readStream.pipe(res);
+    } catch (error) {
+        res.send("not found");
+    }
+});
+
+app.delete("/file/:filename", async (req, res) => {
+    try {
+        await gfs.files.deleteOne({ filename: req.params.filename });
+        res.send("success");
+    } catch (error) {
+        console.log(error);
+        res.send("An error occured.");
+    }
+});
+
 //To listen the server
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
+export default app;
